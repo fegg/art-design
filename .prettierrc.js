c93935d7f79702fe0569eefe09d7f1dae552c8f9
lulin <lulin@g7.com.cn>
2022-10-25 16:14:54 +0800

    chore: init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  bracketSameLine: true,
+};
